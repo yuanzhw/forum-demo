44436b4e38e97d1982315913ef575dbe487cbed0
Yuanzhw <yuanzhw@vip.qq.com>
2019-01-10 14:43:50 +0800

    create basetoolbar

--- src/components/BaseLayout.vue
+++ src/components/BaseLayout.vue
@@ -20,24 +20,7 @@
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar color="indigo" dark fixed app>
-      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
-      <v-toolbar-title>Application</v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-items class="hidden-sm-and-down">
-        <v-btn flat @click="$router.push('/')">首页</v-btn>
-        <v-btn flat v-if="isLogin">
-          <v-badge color="red">
-            <span slot="badge">3</span>
-            <span>未读消息</span>
-          </v-badge>
-        </v-btn>
-        <v-btn flat v-if="isLogin">个人信息</v-btn>
-        <v-btn flat @click="$router.push('/register')" v-if="!isLogin">注册</v-btn>
-        <v-btn flat @click="$router.push('/login')" v-if="!isLogin">登陆</v-btn>
-        <v-btn flat @click="logout" v-if="isLogin">退出</v-btn>
-      </v-toolbar-items>
-    </v-toolbar>
+    <BaseToolbar :isLogin="isLogin" @login="isLogin=$event"></BaseToolbar>
     <v-content>
       <v-layout row>
         <router-view></router-view>
@@ -52,9 +35,11 @@
 </template>
 
 <script>
+import BaseToolbar from './BaseToolbar'
 
 export default {
   components: {
+    BaseToolbar,
   },
   data: () => ({
     drawer: null,
---
+++ src/components/BaseToolbar.vue
@@ -0,0 +1,30 @@
+<template>
+    <v-toolbar color="indigo" dark fixed app>
+      <v-toolbar-side-icon @click.stop="drawer = !drawer"></v-toolbar-side-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items class="hidden-sm-and-down">
+        <v-btn flat @click="$router.push('/')">首页</v-btn>
+        <v-btn flat v-if="isLogin">
+          <v-badge color="red">
+            <span slot="badge">3</span>
+            <span>未读消息</span>
+          </v-badge>
+        </v-btn>
+        <v-btn flat v-if="isLogin">个人信息</v-btn>
+        <v-btn flat @click="$router.push('/register')" v-if="!isLogin">注册</v-btn>
+        <v-btn flat @click="$router.push('/login')" v-if="!isLogin">登陆</v-btn>
+        <v-btn flat @click="$emit('login', false)" v-if="isLogin">退出</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>    
+</template>
+
+<script>
+export default {
+    data: () =>({
+    }),
+    props: {
+        isLogin: Boolean,
+    }
+}
+</script>
